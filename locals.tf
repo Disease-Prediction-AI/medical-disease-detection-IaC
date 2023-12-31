4861958fb66a967f6061336d9df27d80a3dd2a90
reda <reda.himmi@edu.uca.ma>
2024-01-01 00:40:23 +0100

    added IaC code

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env                 = "prod"
+  region              = "eastus2"
+  resource_group_name = "disease-prediction-rg"
+  aks_name            = "disease-prediction-rg"
+  aks_version         = "1.27"
+  acr_name            = "dspredacr"
+}
